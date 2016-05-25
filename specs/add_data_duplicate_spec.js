5b6d2f482e06b6631d0a2eac9adbf3698c01b6fd
Steven Hazel <sah@saucelabs.com>
2016-05-25 13:38:21 -0700

    use walmart.com

--- specs/add_data_duplicate_spec.js
+++ specs/add_data_duplicate_spec.js
@@ -2,7 +2,7 @@
 describe('AngularJS/Protractor Demo Tab1 new', function() {
 
   it('should add data and verify it was added new test', function() {
-    browser.get('http://localhost:8000');
+    browser.get('http://walmart.com/');
 
     element(by.css('.index')).click();
 
