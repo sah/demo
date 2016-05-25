606bd239f189f7dd35ccd54f032820f7c2531c58
Steven Hazel <sah@saucelabs.com>
2016-05-25 14:03:07 -0700

    experimenting with test names

--- conf.js
+++ conf.js
@@ -23,14 +23,13 @@ exports.config = {
     browserName: 'chrome',
     version: '41',
     platform: 'Windows 7',
-    name: "chrome-tests",
     shardTestFiles: true,
     maxInstances: 25
   }, {
     browserName: 'internet explorer',
     version: '11',
     platform: 'Windows 7',
-    name: "ie11-tests",
+    name: "",
     shardTestFiles: true,
     maxInstances: 25
   }],
