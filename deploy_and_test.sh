034140fac0852bb48f8bcd1e37cc6f495b60925f
Steven Hazel <sah@saucelabs.com>
2016-05-26 06:35:54 -0700

    higher parallelism

--- deploy_and_test.sh
+++ deploy_and_test.sh
@@ -17,7 +17,7 @@ pip install -r requirements.txt
 
 # run tests
 echo "Running Functional Tests using py.test"
-if py.test -n20 --boxed demo_tests.py realdev_app_demo_tests.py ; then
+if py.test -n50 --boxed demo_tests.py realdev_app_demo_tests.py ; then
     # shut down server
     kill $FOO_PID
     # return appropriate exit code
