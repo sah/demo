38851625eabb224c3cbba50485f46b9752769cdc
Steven Hazel <sah@saucelabs.com>
2016-05-26 07:19:03 -0700

    put mobile back

--- demo_tests.py
+++ demo_tests.py
@@ -15,7 +15,18 @@ BUILD = os.environ.get('JOB_NAME') + ' #' + os.environ.get('BUILD_NUMBER')
 
 sauce = SauceClient(USERNAME, ACCESS_KEY)
 
-platforms = [
+platforms = [{'deviceName': "Android Emulator",
+              'deviceOrientation': "portrait",
+              'browserName': "android",
+              'version': "5.1"
+              },
+             {'deviceName': "iPhone Simulator",
+              'platformName': "iOS",
+              'platformVersion': "9.2",
+              'deviceOrientation': "portrait",
+              'browserName': "Safari",
+              'appiumVersion': "1.5.2"
+              },
              {'platform': "Windows 10",
               'browserName': "chrome",
               'version': "latest"
