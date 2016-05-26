dec2b8445f8ae6327b7b61c762c753de688eee40
Steven Hazel <sah@saucelabs.com>
2016-05-25 21:18:46 -0700

    Revert "use walmart.com"
    This reverts commit 5b6d2f482e06b6631d0a2eac9adbf3698c01b6fd.

--- specs/default_data_spec.js
+++ specs/default_data_spec.js
@@ -2,7 +2,7 @@
 describe('AngularJS/Protractor Demo Tab1 default values', function() {
 
   it('should have the correct default data', function() {
-    browser.get('http://walmart.com/');
+    browser.get('http://localhost:8000');
 
     element(by.css('.index')).click();
 
